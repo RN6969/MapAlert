e0fe674dce8e2cc8f8790ef088920be28775818b
RN6969 <rnwork964@gmail.com>
2025-04-19 14:33:39 +0530

    update

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DATABASE_URL = "mysql://root:rush6969@34.172.231.232/map-alert-453317:us-central1:map-alert"
+DATABASE_URL = "mysql://mapalert:rush6969@34.172.231.232/map-alert-453317:us-central1:map-alert"
